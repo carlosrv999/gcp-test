c6a8f357730d993026fe10fe2bd20a2ed9132aa9
Carlos Ramirez <carlosrv125@icloud.com>
2022-08-15 15:31:51 -0500

    changes

--- provider.tf
+++ provider.tf
@@ -1,8 +1,10 @@
 terraform {
+  required_version = ">= 1.1.0"
+
   required_providers {
     google = {
       source = "hashicorp/google"
-      version = "4.31.0"
+      version = "~> 4.31.0"
     }
   }
 }
